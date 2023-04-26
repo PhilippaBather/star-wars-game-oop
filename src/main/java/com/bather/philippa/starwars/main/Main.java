39e51b076e390bb98891900c392221a4045dd6e3
pabather <p.a.bather1@gmail.com>
2023-04-26 15:02:23 +0200

    created entry point

---
+++ src/main/java/com/bather/philippa/starwars/main/Main.java
@@ -0,0 +1,10 @@
+package com.bather.philippa.starwars.main;
+
+import com.bather.philippa.starwars.game.Game;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.initiateGame();
+    }
+}
